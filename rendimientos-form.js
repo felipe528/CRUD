23b4d099c2d4b19de03ce0ccb0646d7a60af14e1
felipe528 <anibal.medrano.b@gmail.com>
2022-11-19 18:39:37 -0300

    first commit

---
+++ rendimientos-form.js
@@ -0,0 +1,64 @@
+var DatosTabla = localStorage.getItem ("tablanexo")
+DatosTabla = JSON.parse(DatosTabla)
+if (DatosTabla == null){
+   var DatosTabla = []
+}
+
+var idform = localStorage.getItem("idform")
+ idform = JSON.parse(idform)
+ if (idform == null){
+   var idform = 0
+ }
+
+ cargarPagina ()
+
+ function guardar(){
+   console.log ("presiono guardar")
+   var ObjRsporte = JSON.stringify({
+    codpalnilla: (idform > 0) ? idform : (DatosTabla.length + 1),
+    nombApellido: document.getElementById ("txtnyp").value,
+    email: document.getElementById ("txtmail").value,
+    fono: document.getElementById ("txtfono").value,
+    direccion: document.getElementById ("txtdireccion").value
+   })
+   console.log(ObjRsporte)
+// editar data   
+if (idform > 0) {
+  for (const i in DatosTabla) {
+    var varTabla = JSON.parse(DatosTabla[i])
+    if (varTabla.codpalnilla == idform) {
+    DatosTabla[i] = ObjRsporte
+    break
+
+  }
+}
+
+} else {
+  // new data
+  DatosTabla.push(ObjRsporte)
+  
+}
+localStorage.setItem("tablanexo", JSON.stringify(DatosTabla))
+  window.location.replace("rendimientos.html")
+
+
+ }
+
+ function cargarPagina (){
+   if (idform > 0) {
+    //sacar datos de la tabla
+
+    for (const i in DatosTabla) {
+      var varTabla = JSON.parse(DatosTabla[i])
+      if (varTabla.codpalnilla == idform) {
+          document.getElementById("txtcodigo").value = varTabla.codpalnilla
+          document.getElementById("txtnyp").value = varTabla.nombApellido
+          document.getElementById("txtmail").value = varTabla.email
+          document.getElementById("txtfono").value = varTabla.fono
+          document.getElementById("txtdireccion").value = varTabla.direccion
+          break
+
+    }
+  }
+ }
+}
---
+++ rendimientos.js
@@ -0,0 +1,37 @@
+var DatosTabla = localStorage.getItem ("tablanexo")
+DatosTabla = JSON.parse(DatosTabla)
+if (DatosTabla == null){
+   var DatosTabla = []
+}
+
+function lista (){
+console.log("ingresando a listar");
+var dataFila = ''
+
+if (DatosTabla.length > 0){
+   for(const i in DatosTabla) {
+      var varTabla = JSON.parse (DatosTabla[i])
+      dataFila += "<tr>"
+      dataFila += "<td>"+varTabla.codpalnilla+"</td>"
+      dataFila += "<td>"+varTabla.nombApellido+"</td>"
+      dataFila += "<td>"+varTabla.email+"</td>"
+      dataFila += "<td>"+varTabla.fono+"</td>"
+      dataFila += "<td>"+varTabla.direccion+"</td>"
+      dataFila += "<td>"+
+                  "<button type='button' class='btn btn-warning' onclick='nexoboton("+varTabla.codpalnilla+")'>EDITAR</button>"+
+                  "</td>";
+      
+
+      dataFila += "</tr>"
+   }
+   document.getElementById("datos").innerHTML = dataFila
+}
+
+}
+ function nexoboton (idform){
+    localStorage.setItem("idform", JSON.stringify(idform))
+    window.location.replace ("rendimientos-form.html")
+
+ }
+
+ 
